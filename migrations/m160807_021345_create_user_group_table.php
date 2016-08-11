e7740b989d49358068f9f769f4f2179fcba438f9
hiscaler <hiscaler@gmail.com>
2016-08-11 10:58:19 +0800

    Typo

--- migrations/m160807_021345_create_user_group_table.php
+++ migrations/m160807_021345_create_user_group_table.php
@@ -19,7 +19,7 @@ class m160807_021345_create_user_group_table extends Migration
             'name' => $this->string(30)->notNull()->comment('组头衔'),
             'icon_path' => $this->string(100)->comment('组图标'),
             'min_credits' => $this->integer()->notNull()->defaultValue(0)->comment('最小积分'),
-            'max_credits' => $this->integer()->notNull()->defaultValue(0)->comment('最小积分'),
+            'max_credits' => $this->integer()->notNull()->defaultValue(0)->comment('最大积分'),
             'created_at' => $this->integer()->notNull()->comment('添加时间'),
             'created_by' => $this->integer()->notNull()->comment('添加人'),
             'updated_at' => $this->integer()->notNull()->comment('更新时间'),
