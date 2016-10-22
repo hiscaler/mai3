149d8231e4a7cdf94df8416d7f0d288732add863
hiscaler-PC\hiscaler <hiscaler@gmail.com>
2016-10-23 00:40:01 +0800

    admin: 完善 Controller 基类

--- modules/admin/controllers/Controller.php
+++ modules/admin/controllers/Controller.php
@@ -15,9 +15,9 @@ class Controller extends \yii\web\Controller
     {
         if (parent::beforeAction($action)) {
             // 如果不是管理员并且没有选择管理站点的一律跳到站点选择界面
-//            if (Yad::getUserRole() !== User::ROLE_ADMINISTRATOR && !Yad::getTenantId() && ($this->id != 'default' && !in_array($action->id, ['choice-tenant', 'change-tenant', 'login', 'logout', 'error']))) {
-//                $this->redirect(['/default/choice-tenant']);
-//            }
+            if (!Yad::getTenantId() && ($this->id != 'default' && !in_array($action->id, ['choice-tenant', 'change-tenant', 'login', 'logout', 'error']))) {
+                $this->redirect(['default/choice-tenant']);
+            }
 
             $formatter = Yii::$app->getFormatter();
             $language = Yad::getLanguage();
