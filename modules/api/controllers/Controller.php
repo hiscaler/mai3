20376d61b15fce3bc664cce5828294f7f32b29a2
hiscaler <hiscaler@gmail.com>
2017-05-19 22:05:57 +0800

    api: 完善 Controller 基类

--- modules/api/controllers/Controller.php
+++ modules/api/controllers/Controller.php
@@ -2,10 +2,48 @@
 
 namespace app\modules\api\controllers;
 
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
+use yii\web\Response;
+
 /**
  * 基类
  */
 class Controller extends \yii\rest\Controller
 {
+
+    /**
+     * 序列化数据设置
+     *
+     * @var array
+     */
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::className(),
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'text/javascript' => Response::FORMAT_JSONP,
+                    'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
+            'corsFilter' => [
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 86400, // One day
+                ],
+            ],
+        ];
+    }
 
 }
